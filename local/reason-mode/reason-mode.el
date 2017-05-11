4a1021ef702fb1d423c88a47270f340198418091
Fredrik Dyrkell <fredrik.dyrkell@gmail.com>
2017-05-11 20:09:38 +0200

    Refactor of reason-mode syntax and indent
    - Pulled out all the remains from the rust mode origin
    - Split into files for syntax and indent
    - Merlin keybindings in layer
    - refmt window constraints
    - Experimental utop additions

---
+++ local/reason-mode/reason-mode.el
@@ -0,0 +1,186 @@
+;;; reason-mode.el --- A major emacs mode for editing Reason (based on rust-mode) -*-lexical-binding: t-*-
+;; Portions Copyright (c) 2015-present, Facebook, Inc. All rights reserved.
+
+;; Version: 0.2.0
+;; Author: Mozilla
+;; Url:
+;; Keywords: languages
+
+;; This file is distributed under the terms of both the MIT license and the
+;; Apache License (version 2.0).
+
+(eval-when-compile (require 'rx)
+                   (require 'compile)
+                   (require 'url-vars))
+
+;; for GNU Emacs < 24.3
+(eval-when-compile
+  (unless (fboundp 'setq-local)
+    (defmacro setq-local (var val)
+      "Set variable VAR to value VAL in current buffer."
+      (list 'set (list 'make-local-variable (list 'quote var)) val))))
+
+;; Syntax definitions and helpers
+(defvar reason-mode-syntax-table
+  (let ((table (make-syntax-table)))
+
+    ;; Operators
+    (dolist (i '(?+ ?- ?* ?/ ?& ?| ?^ ?! ?< ?> ?~ ?@))
+      (modify-syntax-entry i "." table))
+
+    ;; Strings
+    (modify-syntax-entry ?\" "\"" table)
+    (modify-syntax-entry ?\\ "\\" table)
+    (modify-syntax-entry ?\' "\"'"  table)
+
+    ;; Comments
+    (modify-syntax-entry ?/  ". 124b" table)
+    (modify-syntax-entry ?*  ". 23n"  table)
+    (modify-syntax-entry ?\n "> b"    table)
+    (modify-syntax-entry ?\^m "> b"   table)
+
+    table))
+
+(defgroup reason-mode nil
+  "Support for Reason code."
+  :link '(url-link "http://facebook.github.io/reason/")
+  :group 'languages)
+
+(defcustom reason-mode-hook nil
+  "Hook called by `reason-mode'."
+  :type 'hook
+  :group 'reason-mode)
+
+;; Font-locking definitions and helpers
+(defconst reason-mode-keywords
+  '("and" "as"
+    "else" "external"
+    "fun" "for"
+    "if" "impl" "in" "include"
+    "let"
+    "module" "match" "mod" "move" "mutable"
+    "open"
+    "priv" "pub"
+    "rec" "ref" "return"
+    "self" "static" "switch" "struct" "super"
+    "trait" "type"
+    "use"
+    "virtual"
+    "where" "when" "while"))
+
+(defconst reason-mode-consts
+  '("true" "false"))
+
+(defconst reason-special-types
+  '("int" "float" "string" "char"
+    "bool" "unit" "list" "array" "exn"
+    "option" "ref"
+))
+
+(defconst reason-camel-case
+  (rx symbol-start
+      (group upper (0+ (any word nonascii digit "_")))
+      symbol-end))
+
+(defconst reason-re-ident "[[:word:][:multibyte:]_][[:word:][:multibyte:]_[:digit:]]*")
+(defun reason-re-word (inner) (concat "\\<" inner "\\>"))
+(defun reason-re-grab (inner) (concat "\\(" inner "\\)"))
+
+;; (See PR #42 -- this is just like `(regexp-opt words 'symbols)` from
+;; newer Emacs versions, but will work on Emacs 23.)
+(defun regexp-opt-symbols (words) (concat "\\_<" (regexp-opt words t) "\\_>"))
+
+;;; Syntax highlighting for Reason
+(setq reason-font-lock-keywords
+      `(
+        (,(regexp-opt-symbols reason-mode-keywords) . font-lock-keyword-face)
+        (,(regexp-opt-symbols reason-special-types) . font-lock-builtin-face)
+        (,(regexp-opt-symbols reason-mode-consts) . font-lock-constant-face)
+
+        (,reason-camel-case 1 font-lock-type-face)
+
+        ;; Field names like `foo:`, highlight excluding the :
+        (,(concat (reason-re-grab reason-re-ident) ":[^:]") 1 font-lock-variable-name-face)
+        ;; Module names like `foo::`, highlight including the ::
+        (,(reason-re-grab (concat reason-re-ident "::")) 1 font-lock-type-face)
+        ;; Name punned labeled args like ::foo
+        (,(concat "[[:space:]]+" (reason-re-grab (concat "::" reason-re-ident))) 1 font-lock-type-face)
+
+        ;; TODO jsx attribs?
+        (,
+         (concat "<[/]?" (reason-re-grab reason-re-ident) "[^>]*" ">")
+         1 font-lock-type-face)
+        ))
+
+(defun reason-mode-try-find-alternate-file (mod-name extension)
+  "Switch to the file given by MOD-NAME and EXTENSION."
+  (let* ((filename (concat mod-name extension))
+         (buffer (get-file-buffer filename)))
+    (if buffer (switch-to-buffer buffer)
+      (find-file filename))))
+
+(defun reason-mode-find-alternate-file ()
+  "Switch to implementation/interface file."
+  (interactive)
+  (let ((name buffer-file-name))
+    (when (string-match "\\`\\(.*\\)\\.re\\([il]\\)?\\'" name)
+      (let ((mod-name (match-string 1 name))
+            (e (match-string 2 name)))
+        (cond
+         ((string= e "i")
+          (reason-mode-try-find-alternate-file mod-name ".re"))
+         (t
+          (reason-mode-try-find-alternate-file mod-name ".rei")))))))
+
+
+(defalias 'reason-parent-mode
+  (if (fboundp 'prog-mode) 'prog-mode 'fundamental-mode))
+
+(require 'reason-indent)
+
+;;;###autoload
+(define-derived-mode reason-mode reason-parent-mode "Reason"
+  "Major mode for Reason code.
+
+\\{reason-mode-map}"
+  :group 'reason-mode
+  :syntax-table reason-mode-syntax-table
+
+  ;; Indentation
+  (setq-local indent-line-function 'reason-mode-indent-line)
+  ;; Fonts
+  (setq-local font-lock-defaults '(reason-font-lock-keywords))
+  ;; Misc
+  (setq-local comment-start "/*")
+  (setq-local comment-end   "*/")
+  (setq-local indent-tabs-mode nil)
+  ;; Allow paragraph fills for comments
+  (setq-local comment-start-skip "/\\*+[ \t]*")
+  (setq-local paragraph-start
+              (concat "^[ \t]*$\\|\\*)$\\|" page-delimiter))
+  (setq-local paragraph-separate paragraph-start)
+  (setq-local require-final-newline t)
+  (setq-local normal-auto-fill-function nil)
+  (setq-local comment-multi-line t)
+
+  (setq-local beginning-of-defun-function 'reason-beginning-of-defun)
+  (setq-local end-of-defun-function 'reason-end-of-defun)
+  (setq-local parse-sexp-lookup-properties t)
+  )
+
+;;;###autoload
+(add-to-list 'auto-mode-alist '("\\.rei?\\'" . reason-mode))
+
+(defun reason-mode-reload ()
+  (interactive)
+  (unload-feature 'reason-mode)
+  (unload-feature 'reason-indent)
+  (require 'reason-mode)
+  (reason-mode))
+
+(require 'refmt)
+(provide 'reason-mode)
+
+;;; reason-mode.el ends here
+
+;;; TODO: keymaps, e.g. (define-key map "\C-c\C-a" 'reason-mode-find-alternate-file)
--- packages.el
+++ packages.el
@@ -11,6 +11,7 @@
 
 (defconst reason-packages
   '(company
+    utop
     (reason-mode :location local)
     merlin))
 
@@ -20,9 +21,25 @@
 (defun reason/init-reason-mode ()
   (use-package reason-mode
     :mode ("\\.re\\'" . reason-mode)
-    :config
+    :init
     (progn
-      (add-hook 'reason-mode-hook 'merlin-mode)
+      ;; (require 'utop)
+      (add-hook 'reason-mode-hook (lambda ()
+                                    (add-hook 'before-save-hook 'refmt-before-save)
+                                    (merlin-mode)
+                                    (utop-minor-mode)))
+      (setq utop-command "opam config exec -- rtop -emacs")
+
+      (evil-leader/set-key-for-mode 'reason-mode "<SPC>" 'company-complete)
+
+      ;; (setq refmt-show-errors 'echo)
+      (add-to-list 'display-buffer-alist
+                   `(,(rx bos "*Refmt Errors*" eos)
+                     (display-buffer-pop-up-window display-buffer-in-side-window)
+                     (reusable-frames . visible)
+                     (side            . bottom)
+                     (window-height   . 0.15)))
+
       )))
 
 (defun reason/post-init-merlin ()
@@ -33,6 +50,25 @@
       (setq merlin-completion-with-doc t)
       (setq merlin-ac-setup t)
       (push 'merlin-company-backend company-backends-merlin-mode)
+
+      (spacemacs/set-leader-keys-for-major-mode 'reason-mode
+        "cp" 'merlin-project-check
+        "cv" 'merlin-goto-project-file
+        "eC" 'merlin-error-check
+        "en" 'merlin-error-next
+        "eN" 'merlin-error-prev
+        "gb" 'merlin-pop-stack
+        "gg" 'merlin-locate
+        "gG" 'spacemacs/merlin-locate-other-window
+        "gl" 'merlin-locate-ident
+        "gi" 'merlin-switch-to-ml
+        "gI" 'merlin-switch-to-mli
+        "go" 'merlin-occurrences
+        "hh" 'merlin-document
+        "ht" 'merlin-type-enclosing
+        "hT" 'merlin-type-expr
+        "rd" 'merlin-destruct)
+
       )))
 
 ;;; packages.el ends here
